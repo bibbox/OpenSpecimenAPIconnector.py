3c2451ea80ea920eebf51799b71cd0abe4f91436
ChristianMUG <christian.schorn@medunigraz.at>
2021-01-19 11:09:36 +0100

    changed workflow of sites accordingly

--- os_core/jsons.py
+++ os_core/jsons.py
@@ -360,5 +360,27 @@ class Json_factory():
         }
 
         return json.dumps(data)
+
+    def site_search_url_gen(self, sitename=None, institutename=None, maxresults=100):
+
+        url_string = '/?'
+
+        if sitename!=None:
+            if isinstance(sitename,list):
+                for name in sitename:
+                    url_string += 'name=' + str(name).replace(' ', '+') + '&'
+            else:
+                url_string += 'name=' + str(sitename).replace(' ', '+') + '&'
+
+        if institutename!=None:
+            if isinstance(institutename,list):
+                for name in institutename:
+                    url_string += 'institute=' + str(name).replace(' ','+') + '&'
+            else:
+                url_string += 'institute=' + str(institutename).replace(' ','+') + '&'
+        
+        url_string += 'maxResults=' + str(maxresults)
+
+        return url_string
     
 
