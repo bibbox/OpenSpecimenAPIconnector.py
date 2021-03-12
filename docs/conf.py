e2df41989d4a04601ea3251813fa853672a7dd16
Simon <simon.streit@student.tugraz.at>
2021-03-12 14:40:20 +0100

    add read the docs first try

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 import dateutil
-sys.path.insert(0, os.path.abspath('../src/'))
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
