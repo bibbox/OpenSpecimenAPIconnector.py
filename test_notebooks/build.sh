842720ef5ebbdcad12dda6a08bb2e9c66cbdea6f
Christoph Josef Koch <christoph.koch@medunigraz.at>
2025-08-27 14:26:38 +0200

    generic version of build file

--- test_notebooks/build.sh
+++ test_notebooks/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 cd ../
-python setup.py bdist_wheel
-cp dist/OpenSpecimenAPIconnector-0.9.3-py3-none-any.whl ../os-datahandler/dep/OpenSpecimenAPIconnector-0.9.3-py3-none-any.whl
-/home/christoph/git/os-datahandler/.venv/bin/python -m pip install --force-reinstall /home/christoph/git/os-datahandler/dep/OpenSpecimenAPIconnector-0.9.3-py3-none-any.whl
+/usr/bin/python setup.py bdist_wheel
+#cp dist/OpenSpecimenAPIconnector-0.9.4-py3-none-any.whl ../os-datahandler/dep/OpenSpecimenAPIconnector-0.9.4-py3-none-any.whl
+#/home/christoph/git/os-datahandler/.venv/bin/python -m pip install --force-reinstall /home/christoph/git/os-datahandler/dep/OpenSpecimenAPIconnector-0.9.4-py3-none-any.whl
